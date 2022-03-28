c9d9fd285de285fc62347ca8721835f27ddd5b4e
Kinekenneth48 <kinekenneth48@gmail.com>
2022-03-27 19:52:12 -0600

    add script to compute distances

---
+++ script/compute_distance.R
@@ -0,0 +1,23 @@
+
+# load libraries, function and data
+library(googleway)
+source("R/get_dist.R")
+load("data-raw/RObject/flow_data.RData")
+
+
+
+#rename data
+flow_data_list <- flow_data
+
+
+# create index
+flow_data_list <- flow_data_list %>%
+  dplyr::group_by(index) %>%
+  dplyr::group_split()
+
+
+# compute distance of flow data
+flow_data_dist <- do.call(rbind, lapply(flow_data_list, get_dist))
+
+#save data as RObject
+save(flow_data_dist, file= "data-raw/RObject/flow_data_dist.RData")
