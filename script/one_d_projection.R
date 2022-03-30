13ca1643d50edbce368a9d23b0e98279e28ccd59
Tom Kerby <46465501+tjkerby@users.noreply.github.com>
2022-03-30 15:43:40 -0600

    Calc distances between stations and create line of I-15

---
+++ script/one_d_projection.R
@@ -0,0 +1,35 @@
+load("data-raw/RObject/flow_data.RData")
+crash <- read.csv("data-raw/RawCrashData2020.csv")
+
+plot(flow_data$Longitude, flow_data$Latitude)
+
+
+get_dist <- function(flow_data) {
+  lat_data <- flow_data[order(flow_data$Latitude), ]
+  dist <- c(0)
+  dist_station_south <- c(0)
+  for(i in 2:length(lat_data$Latitude)) {
+    new_dist <- sqrt((lat_data$Longitude[i] - lat_data$Longitude[i - 1])^2 + 
+      (lat_data$Latitude[i] - lat_data$Latitude[i - 1])^2) # Euclidean distance
+    dist <- c(dist, new_dist + dist[i - 1])
+    dist_station_south <- c(dist_station_south, new_dist)
+  }
+  list("dist" = dist, "dist_to_station" = dist_station_south)
+}
+
+flow_data <- flow_data[order(flow_data$Latitude), ]
+results <- get_dist(flow_data)
+flow_data$dist_to_bottom <- results[["dist"]]
+flow_data$dist_to_station <- results[["dist_to_station"]]
+flow_data$id <- seq(1:nrow(flow_data))
+
+station_points <- st_as_sf(flow_data,
+                 coords = c("Longitude", "Latitude"),
+                 crs = 4269)
+station_points <- station_points %>% arrange(id)
+
+I_15_line <- station_points %>% summarise(do_union = FALSE) %>% st_cast("LINESTRING")
+plot(I_15_line)
+
+save(station_points, file = "data-raw/RObject/station_points.RData")
+save(I_15_line, file = "data-raw/RObject/I_15_line.RData")
