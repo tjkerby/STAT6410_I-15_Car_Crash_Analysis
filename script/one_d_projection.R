12fdf145f10f72fcc1e18bb664e237c075d60488
Tom Kerby <46465501+tjkerby@users.noreply.github.com>
2022-03-31 14:06:23 -0600

    fixed units

--- script/one_d_projection.R
+++ script/one_d_projection.R
@@ -1,24 +1,36 @@
 load("data-raw/RObject/flow_data.RData")
 crash <- read.csv("data-raw/RawCrashData2020.csv")
 
+bottom_crash <- st_as_sf(crash[crash$Lat == min(crash$Lat), ],
+         coords = c("Long", "Lat"), crs = 4269)
+
 plot(flow_data$Longitude, flow_data$Latitude)
 
-
-get_dist <- function(flow_data) {
+get_dist <- function(bottom_crash, flow_data) {
   lat_data <- flow_data[order(flow_data$Latitude), ]
-  dist <- c(0)
-  dist_station_south <- c(0)
-  for(i in 2:length(lat_data$Latitude)) {
-    new_dist <- sqrt((lat_data$Longitude[i] - lat_data$Longitude[i - 1])^2 + 
-      (lat_data$Latitude[i] - lat_data$Latitude[i - 1])^2) # Euclidean distance
-    dist <- c(dist, new_dist + dist[i - 1])
-    dist_station_south <- c(dist_station_south, new_dist)
+  lat_data$id <- seq(1:nrow(lat_data))
+  lat_data <- st_as_sf(lat_data,
+           coords = c("Longitude", "Latitude"),
+           crs = 4269)
+  dist <- c()
+  dist_station_south <- c()
+  i = 1
+  for (i in 1:nrow(lat_data)) {
+    if(i == 1) {
+      new_dist <- sf::st_distance(bottom_crash, lat_data[i,])
+      dist <- c(new_dist)
+      dist_station_south <- c(new_dist)
+    } else {
+      new_dist <- sf::st_distance(lat_data[i-1,], lat_data[i,])
+      dist <- c(dist, new_dist + dist[i - 1])
+      dist_station_south <- c(dist_station_south, new_dist)
+    }
   }
   list("dist" = dist, "dist_to_station" = dist_station_south)
 }
 
 flow_data <- flow_data[order(flow_data$Latitude), ]
-results <- get_dist(flow_data)
+results <- get_dist(bottom_crash, flow_data)
 flow_data$dist_to_bottom <- results[["dist"]]
 flow_data$dist_to_station <- results[["dist_to_station"]]
 flow_data$id <- seq(1:nrow(flow_data))
